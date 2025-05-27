888eb85ac28d424cd04c8219337847d74874924c
eudivan <eudivan44@gmail.com>
2025-05-27 14:08:45 -0300

    feat: created category model

--- config/settings/base.py
+++ config/settings/base.py
@@ -6,6 +6,10 @@ from pathlib import Path
 
 import environ
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 # poupeai_finance_service/
 APPS_DIR = BASE_DIR / "poupeai_finance_service"
--- poupeai_finance_service/categories/models.py
+++ poupeai_finance_service/categories/models.py
@@ -1,1 +1,18 @@
-# Create your models here.
+from poupeai_finance_service.core.models import TimeStampedModel
+from poupeai_finance_service.users.models import Profile
+from django.db import models
+
+class Category(TimeStampedModel):
+    CATEGORY_TYPES = (
+        ('expense', 'Despesa'),
+        ('income', 'Receita')
+    )
+    name = models.CharField(max_length=30, verbose_name="Category Name", 
+                            blank=False, null=False)
+    color_hex = models.CharField(max_length=7, verbose_name='Category Color', 
+                                 default='#000000', null=False, blank=False)
+    type = models.CharField(max_length=7, verbose_name="Category Type", choices=CATEGORY_TYPES)
+    profile_id = models.ForeignKey(to=Profile, verbose_name='User', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
