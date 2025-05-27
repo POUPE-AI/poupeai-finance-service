d8c5bbac777fe16f8682ad189324f14bb3d406ad
Francisco Paulino de Queiroz <franciscopaulino.q@gmail.com>
2025-05-26 21:13:24 -0300

    feat: Define CustomUser and Profile models

--- poupeai_finance_service/users/models.py
+++ poupeai_finance_service/users/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    """
+    Custom user model extending Django's AbstractUser.
+    """
+
+    class Meta:
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
+        ordering = ['username']
+
+    pass
+
+class Profile(models.Model):
+    """
+    User profile model to store additional data.
+    One-to-one relationship with CustomUser.
+    """
+    GENDER_CHOICES = [
+        ('M', _('Male')),
+        ('F', _('Female')),
+        ('O', _('Other')),
+    ]
+
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
+    gender = models.CharField(_('Gender'), max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
+    date_of_birth = models.DateField(_('Date of Birth'), blank=True, null=True)
+
+    class Meta:
+        verbose_name = _('Profile')
+        verbose_name_plural = _('Profiles')
+        ordering = ['user__username']
+
+    def __str__(self):
+        return f'Profile of {self.user.username}'
