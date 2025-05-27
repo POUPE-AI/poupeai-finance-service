9886aad6ef181bb5d7866ff980c754ba52be9b28
Francisco Paulino de Queiroz <franciscopaulino.q@gmail.com>
2025-05-26 22:03:59 -0300

    feat: Include 'users' app URLs in project's main URLs

--- poupeai_finance_service/users/urls.py
+++ poupeai_finance_service/users/urls.py
@@ -1,5 +1,15 @@
-from django.urls import path, include
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from users.api.views import RegisterUserAPIView, UserProfileAPIView
+
+app_name = "users"
 
 urlpatterns = [
-    path("api/v1/users/", include("users.api.urls", namespace="users")),
+    path('register/', RegisterUserAPIView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('me/', UserProfileAPIView.as_view(), name='user_profile'),
 ]
