978906eca0a3034bad1f58f62393b12831d2ecc4
Erisvaldo <erisvaldoapps@gmail.com>
2025-05-27 21:28:06 -0300

    feat: created bank account model

--- poupeai_finance_service/bank_accounts/models.py
+++ poupeai_finance_service/bank_accounts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+from poupeai_finance_service.core.models import TimeStampedModel
+from poupeai_finance_service.users.models import Profile
+
+class BankAccount(TimeStampedModel):
+    name = models.CharField(_('Name'), max_length=50)
+    description = models.TextField(_('Description'), blank=True)
+    initial_balance = models.DecimalField(
+        _('Initial Balance'),
+        max_digits=10, 
+        decimal_places=2, 
+        default=0,
+        validators=[MinValueValidator(0, message='Initial balance cannot be negative')]
+    )
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='bank_accounts')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'profile'],
+                name='unique_bank_account_name_per_profile'
+            )
+        ]
+
+        verbose_name = _('Bank Account')
+        verbose_name_plural = _('Bank Accounts')
+
+    def __str__(self):
+        return f'Bank Account: {self.name}'
+    
