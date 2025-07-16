55d9bda1752194f7e5365eafa12995cc294b921a
Erisvaldo <erisvaldoapps@gmail.com>
2025-07-16 18:51:22 -0300

    feat: change users endpoint to profile

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
         name="api-docs",
     ),
 
-    path('api/v1/users/', include('users.urls', namespace='users')),
+    path('api/v1/profile/', include('profiles.urls', namespace='profiles')),
     path('api/v1/categories/', include('categories.urls', namespace='categories')),
     path('api/v1/bank-accounts/', include('bank_accounts.urls', namespace='bank-accounts')),
     path("api/v1/credit-cards/", include("credit_cards.urls", namespace="credit_cards")),
